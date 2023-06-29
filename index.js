557c612ffa24c7775756c64c12b42c3fbfe972fb
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-06-29 15:40:17 +0530

    Arithmatic Oparations

--- index.js
+++ index.js
@@ -15,21 +15,42 @@
             2. Assignment (= assignment operator)
 */
 
-let firstname="Avishka";  //string data type 
-let age;    // number data type 
-let student= false; //boolean
-age=22;
+// let firstname="Avishka";  //string data type 
+// let age;    // number data type 
+// let student= false; //boolean
+// age=22;
 
-let newage = age+3
-
-
-console.log("Hello" ,firstname);
-console.log("You are",age,"years old");
-console.log (student);
-console.log ("after 3 years your age is =",newage);
+// let newage = age+3
 
 
-document.getElementById("p1").innerHTML = 'Hello ' + firstname;
-document.getElementById("p2").innerHTML = 'You Are '+age+ ' Years Old';
-document.getElementById("p3").innerHTML = "After 3 Years, Your Age is = " +newage;
+// console.log("Hello" ,firstname);
+// console.log("You are",age,"years old");
+// console.log (student);
+// console.log ("after 3 years your age is =",newage);
+
+
+// document.getElementById("p1").innerHTML = 'Hello ' + firstname;
+// document.getElementById("p2").innerHTML = 'You Are '+age+ ' Years Old';
+// document.getElementById("p3").innerHTML = "After 3 Years, Your Age is = " +newage;
+
+/* Arithmetic Expressions 
+    - Arithmetic Expression is a combination of 
+        - operands (values, variables, etc..)
+        - oparators (+ - * / %)
+        - That can be avaluated to a value 
+        - ex y=x+5
+*/
+
+//Oparators 
+// let students=20;
+
+// // students=students+1; add   students +=1;
+// // students=students-1;  substract  students -=1;
+// // students=students*2; multiply students *=1;
+// // students=students /2; divide   students /=1;
+
+// // let extrastudents = students%3; remainer 
+
+// console.log(students);
+// console.log(extrastudents);
 
